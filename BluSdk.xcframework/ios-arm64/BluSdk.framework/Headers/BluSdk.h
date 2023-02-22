fe01a3dc06e8f6bfcdaaf8aa3d1f63763adb56d3
Bintang Samosir <bintang.samosir@bcadigital.co.id>
2023-02-22 14:32:11 +0700

    Add framework

---
+++ BluSdk.xcframework/ios-arm64/BluSdk.framework/Headers/BluSdk.h
@@ -0,0 +1,19 @@
+//
+//  BluSdk.h
+//  BluSdk
+//
+//  Created by Muhammad Nur Hidayat on 10/12/20.
+//  
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BluSdk.
+FOUNDATION_EXPORT double BluSdkVersionNumber;
+
+//! Project version string for BluSdk.
+FOUNDATION_EXPORT const unsigned char BluSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BluSdk/PublicHeader.h>
+
+
